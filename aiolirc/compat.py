e41a9d1a2ff0534c5872f61ce8b75f833a446c70
vahid <vahid@dobisel.com>
2016-09-24 02:24:40 +0330

    aiter compatibility

--- aiolirc/compat.py
+++ aiolirc/compat.py
@@ -8,7 +8,7 @@ if sys.version_info < (3, 5, 2):
         @functools.wraps(instance.__aiter__)
         async def wrapper(self):
             return self
-        return wrapper
+        return wrapper()
 else:
     def aiter_compat(instance):
         return instance
